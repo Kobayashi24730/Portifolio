082217c4fcae2f3c949e2e42427685fae98e74b7
Kobayashi24730 <ajenteduplo000@gmail.com>
2025-12-08 19:17:22 -0300

    Remove Footer component from App.jsx

--- frontend/src/pages/App.jsx
+++ frontend/src/pages/App.jsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../components/Home.jsx";
 import Navbar from "../components/Navbar.jsx";
-//import Footer from "../components/Footer.jsx"'
 import TemaContext from "../components/TemaContext";
 
 export default function App() {
@@ -11,12 +10,10 @@ export default function App() {
   return (
     <BrowserRouter>
       <TemaContext.Provider value={{ temar, setTemar }}>
- 
         <Navbar />
 
         <Routes>
           <Route path="/lar" element={<Home />} />
-          <Route path="/rodape" element={<Footer />} />
         </Routes>
 
         <Footer />
