9eb1b6efa93e128bd37a42481bb58a7b1505820e
Kobayashi24730 <ajenteduplo000@gmail.com>
2025-12-08 18:47:30 -0300

    Fix variable declaration and export order in TemaContext

--- frontend/src/components/TemaContext.jsx
+++ frontend/src/components/TemaContext.jsx
@@ -1,4 +1,4 @@
-import {createContext} from "react";
+import { createContext } from "react";
 
-const temaContext = createContext();
+const TemaContext = createContext();
 export default TemaContext;
