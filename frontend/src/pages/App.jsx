fcc186b628102c7d570ce5fa1487402537431882
Kobayashi24730 <ajenteduplo000@gmail.com>
2025-12-08 19:29:12 -0300

    Fix state variable naming in App component

--- frontend/src/pages/App.jsx
+++ frontend/src/pages/App.jsx
@@ -3,13 +3,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../components/Home.jsx";
 import Navbar from "../components/Navbar.jsx";
 import TemaContext from "../components/TemaContext";
+import Footer from "../components/Footer.jsx";
 
 export default function App() {
-  const [temar, setTemar] = useState("escuro");
+  const [tema, setTema] = useState("escuro");
 
   return (
     <BrowserRouter>
-      <TemaContext.Provider value={{ temar, setTemar }}>
+      <TemaContext.Provider value={{ tema, setTema }}>
         <Navbar />
 
         <Routes>
