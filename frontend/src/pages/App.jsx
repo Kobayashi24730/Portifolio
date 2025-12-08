29eccf073135cbd92327b45cf44665fec823fe56
Kobayashi24730 <ajenteduplo000@gmail.com>
2025-12-08 19:08:09 -0300

    Remove unused imports in App.jsx

--- frontend/src/pages/App.jsx
+++ frontend/src/pages/App.jsx
@@ -1,4 +1,4 @@
-import { useContext ,createContext, useState } from "react";
+import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../components/Home.jsx";
 import Navbar from "../components/Navbar.jsx";
